5f6ccf53f204a814a38c87d649d1d679e6f6eef4
Nick Econopouly <nick@mail.wrycode.com>
2024-10-04 10:47:18 -0400

    Fix tests

--- tests/e2e.py
+++ tests/e2e.py
@@ -7,4 +7,4 @@ backend_url = os.environ["BACKEND_URL"]
 def test_hello_world_response():
     response = requests.get(backend_url)
     assert response.status_code == 200, "Expected status code 200"
-    assert "Hello World!" in response.text , "Response does not contain 'Hello World!"
+    assert "Hello, world!" in response.text , "Response does not contain 'Hello World!"
--- tests/main_test.py
+++ tests/main_test.py
@@ -16,24 +16,15 @@ import os
 
 import pytest
 
-import main
+import app
 
 @pytest.fixture
 def client():
-    main.app.testing = True
-    return main.app.test_client()
-
+    app.app.testing = True
+    return app.app.test_client()
 
 def test_handler_no_env_variable(client):
     r = client.get("/")
 
-    assert r.data.decode() == "Hello World!"
-    assert r.status_code == 200
-
-
-def test_handler_with_env_variable(client):
-    os.environ["NAME"] = "Foo"
-    r = client.get("/")
-
-    assert r.data.decode() == "Hello Foo!"
+    assert r.data.decode() == "Hello, world!"
     assert r.status_code == 200
